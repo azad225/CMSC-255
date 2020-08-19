9710e2a2261c7051372adc16e5c82299cbd8fda0
azad225 <69925907+azad225@users.noreply.github.com>
2020-08-19 16:19:43 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,18 @@
+/******************************************************
+* Hello.java
+* Niaz Azad
+* 08/19/2020
+* CMSC 255- C 90
+******************************************************/
+
+public class Hello {
+	public static void main (String[] args) {
+	System.out.println("Niaz Azad");
+	System.out.println("Hello Java");
+	System.out.println(" This program displays my name and other details");
+	System.out.println(" 08/19/2020");
+	System.out.println("CMSC 255- C 90");
+	System.out.println();
+	
+	}
+}
